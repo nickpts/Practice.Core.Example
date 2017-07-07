a32b19a68b0e439c26161054d33856cd06cc8683
Nick <nicholas.patsaris@gmail.com>
2017-07-07 22:22:58 +0100

    7.0 features

--- Practice.Core.Examples/Features/LangFeatures.cs
+++ Practice.Core.Examples/Features/LangFeatures.cs
@@ -14,19 +14,20 @@ namespace Practice.Core.Examples.Features
     {
         public LangFeatures() { }
 
+        #region C# 6.0
+
         // read-only auto property
         public string TestReadOnlyProperty => string.Empty;
 
         // automatic initialization for auto property
         public string TestProperty { get; set; } = string.Empty;
 
-
         public static void CSharp6()
         {
             Stock item = new Stock("*");
             var name = nameof(item);
 
-            //will display "item";
+            // will display "item";
             Console.WriteLine(name);
 
             item = null;
@@ -37,7 +38,52 @@ namespace Practice.Core.Examples.Features
         // expression-bodied method with return type
         public string ReturnSomethingInteresting() => string.Empty;
 
-        // expression-bodied void method 
-        public void DoSomethingInteresting() => Console.WriteLine("test"); 
+        // expression-bodied method with no return type
+        public void DoSomethingInteresting() => Console.WriteLine("test");
+
+        #endregion
+
+        #region C# 7.0
+
+        // _ can now appear as a separator in literals
+        int x = 123_456;
+
+        // binary literal - no need to know remember hexadecimal
+        byte b1 = 0b00101101;
+
+        // expression-bodied constructor
+        public LangFeatures(string inputVariable) => inputVariable = "Test";
+
+        // expression-bodied constructor with throw statement
+        public LangFeatures(string inputVariable, string testVariable) => TestProperty = testVariable ?? throw new ArgumentException(nameof(testVariable));
+
+        // expression-bodied property
+        public string TestExpProp
+        {
+            get => "test";
+            set => TestProperty = value;
+        }
+
+        // expression-bodied method with throw
+        public string TestThrowMethod() => throw new NotImplementedException();
+
+        // throw expression
+        public string TestTernaryWithThrow(string testInput)
+        {
+            return testInput.Length > 0 ? testInput : throw new InvalidOperationException();
+        }
+        
+
+        #endregion
+
+        #region Private implementation
+
+        private static void TestOutMethod(int x, int y)
+        {
+
+        }
+
+        #endregion
+
     }
 }
